fe2d9cb89d5ccbfef1fe172ef2b0e4931cfd0cbf
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2023-02-16 17:55:54 +0700

    belajar variable

---
+++ src/LearnVariable.java
@@ -0,0 +1,14 @@
+public class LearnVariable {
+    public static void main(String[] args) {
+        // dapat dilakuakn seperit let pada javascript dan sisanya aturan nya sama
+
+        String name="Nurfadizy";
+        name="Riyamizrd";
+        int age = 22;
+
+        String total = name +" Umur "+ age;
+
+        System.out.println(total);
+        
+    }
+}
