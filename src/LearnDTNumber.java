b4b41216e3e9327dd7c8aec37684502b2735aa5c
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2023-02-16 16:37:18 +0700

    Jenis number

---
+++ src/LearnDTNumber.java
@@ -0,0 +1,20 @@
+public class LearnDTNumber {
+    public static void main(String[] args) {
+        // type
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 1000000;
+        long iniLong = 1000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        int decimalInt = 34;
+        int hexadecimal = 0xFFFFFF;
+        int bincaryDecimal = 0b1010101;
+
+        int amount = 1_000_000_000;
+
+    }
+
+}
