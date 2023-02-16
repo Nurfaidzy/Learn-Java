87b61a650bd43d76e659da705765eeae3e046632
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2023-02-16 17:40:29 +0700

    Learn mengenai tipe data char

---
+++ src/LearnCharacter.java
@@ -0,0 +1,13 @@
+public class LearnCharacter {
+    public static void main(String[] args) {
+        // aturan nya adalah dengan menggunakan petik satu untuk devenisinya
+        char siN = 'N';
+        char siU = 'U';
+        char siR = 'R';
+
+        System.out.print(siN);
+        System.out.print(siU);
+        System.out.print(siR);
+
+    }
+}
