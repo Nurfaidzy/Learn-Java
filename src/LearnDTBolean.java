31e4a310044c7f6751196ed8990f54f2ab25e7b9
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2023-02-16 17:48:00 +0700

    learn data type bolean dan string

---
+++ src/LearnDTBolean.java
@@ -0,0 +1,6 @@
+public class LearnDTBolean {
+    public static void main(String[] args) {
+        boolean satu = true;
+        boolean dua = false;
+    }
+}
---
+++ src/LearnDTString.java
@@ -0,0 +1,10 @@
+public class LearnDTString {
+    public static void main(String[] args) {
+        // petiknya harus 2 untuk string
+        String nama = "Nurfaidzy";
+        String nama2="Riyamizard";
+        String FullName = nama + "" + nama2;
+        System.out.println(FullName);
+    }
+    
+}
