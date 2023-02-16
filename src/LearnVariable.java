35df808eab88919506f134cf42c9a3161af5cf69
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2023-02-16 18:00:39 +0700

    belajar variabel pada java

--- src/LearnVariable.java
+++ src/LearnVariable.java
@@ -2,13 +2,23 @@ public class LearnVariable {
     public static void main(String[] args) {
         // dapat dilakuakn seperit let pada javascript dan sisanya aturan nya sama
 
-        String name="Nurfadizy";
-        name="Riyamizrd";
+        String name = "Nurfadizy";
+        name = "Riyamizrd";
         int age = 22;
 
-        String total = name +" Umur "+ age;
+        String total = name + " Umur " + age;
 
         System.out.println(total);
-        
+
+        // ada juga penggunaan var tapi dengan syarat harus langsung diberikan
+        // definisinya
+        // sepertinya hanya berlaku pada penggunaan string saja
+        var kerja = "akulaku";
+
+        // ada final dimana tidak akan dapat diubah2 diselanjutnya dan bersifat statis
+        final String marga = "Saragih";
+
+        System.out.println(kerja + " " +marga);
+
     }
 }
