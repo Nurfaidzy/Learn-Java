4364af0718c2eaac6da2adb1448adf14fc664d77
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2023-02-17 13:56:18 +0700

    learn matematic operations

---
+++ src/LearnMatematicOperations.java
@@ -0,0 +1,9 @@
+public class LearnMatematicOperations {
+    public static void main(String[] args) {
+        // sama saja dengan programming lainnya 
+        int a =100;
+        int b =10;
+        int hasil = a / b;
+        System.out.print(hasil);
+    }
+}
