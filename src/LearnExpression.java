9b4613e7f54a8c0b06387061d7b2a468c764a3da
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2023-02-17 14:06:41 +0700

    pengenalan block, statment, dan ekspression

---
+++ src/LearnExpression.java
@@ -0,0 +1,15 @@
+public class LearnExpression {
+    public static void main(String[] args) {
+        // ekspression digunakan untuk melakukan pendefenisian dari variable
+        int value;
+        value = 10;
+        System.out.print(value == 100);
+
+        // statement
+        // sytem.out.print() ini adalah ekspresi krena dalam print() ada statemen atau
+        // dapat dikatakna bahwa ini merupakan kumpulan dari statement
+        System.out.print(value == 100);
+
+        // block dapat dikatakan sebagai block ketika di mulai dari  { hingg } 
+     }
+}
