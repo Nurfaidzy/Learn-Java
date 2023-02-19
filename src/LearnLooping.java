685c801da76e372efc10731a5ca277108efd1fe8
nurfaidzy <nurfaidzy@gmail.com>
2023-02-19 18:18:38 +0700

    Learn looping and return of method

---
+++ src/LearnLooping.java
@@ -0,0 +1,11 @@
+public class LearnLooping {
+    public static void main(String[] args) {
+        String[] kumpulanNama = {"nurfaidzy","Riyamizard","Saragih"};
+
+        for (String x : kumpulanNama) {
+            System.out.println(x);
+            
+        }
+        
+    }
+}
---
+++ src/LearnMethod.java
@@ -0,0 +1,16 @@
+public class LearnMethod {
+
+    static String marga(String a){
+        return a;
+    }
+
+    static void namaSaya(String a, String b) {
+        String x = a + " "+ b;
+        System.out.print(x);
+    }
+
+    public static void main(String[] args) {
+        namaSaya("Nurfaidzy","Riyamizard");
+        System.out.println( marga("Saragih"));;
+    }
+}
